c5bd3f173234505477854f17aba797004a090e7a
say ken <a.meowalien@gmail.com>
2024-06-23 15:21:05 +0800

    add 3195

---
+++ 3195_Find_the_Minimum_Area_to_Cover_All_Ones_I/solution_test.go
@@ -0,0 +1,21 @@
+package _195_Find_the_Minimum_Area_to_Cover_All_Ones_I
+
+import "testing"
+
+func TestSolution1(t *testing.T) {
+	grid := [][]int{{0, 1, 0}, {1, 0, 1}}
+	expected := 6
+	got := minimumArea(grid)
+	if got != expected {
+		t.Fatalf("got: %v, expected: %v", got, expected)
+	}
+}
+
+func TestSolution2(t *testing.T) {
+	grid := [][]int{{0, 0}, {1, 0}}
+	expected := 1
+	got := minimumArea(grid)
+	if got != expected {
+		t.Fatalf("got: %v, expected: %v", got, expected)
+	}
+}
